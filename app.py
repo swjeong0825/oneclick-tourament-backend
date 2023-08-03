152085b4c2adcc60f0d8dfdf55ffd1faf3e43e3a
sunwoojeong <sunwoojeongcoding@gmail.com>
2023-08-03 04:27:55 -0700

    create_simple_apis_for_tournament

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from routers.Tournament.tournament_router import tournament_blueprint
+
 app = Flask(__name__)
+app.register_blueprint(tournament_blueprint, url_prefix='/tournament')
 
 @app.route("/")
 def hello():
