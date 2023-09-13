98778835508592664b4ebb0446983285125399c8
sunwoojeong <sunwoojeongcoding@gmail.com>
2023-09-12 20:00:19 -0700

    add gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,13 @@
-bind = "0.0.0.0:8080"
-workers = 2
+import multiprocessing
+
+bind = "0.0.0.0:8000"  # Bind the application to this address and port
+
+# Number of worker processes to spawn
+workers = multiprocessing.cpu_count() * 2 + 1
+
+# Module name of the Flask application
+# Make sure this matches the name used in your `application.py` file
+module_name = "app"
+
+# Name of the callable within the Flask application
+callable_name = "app"
